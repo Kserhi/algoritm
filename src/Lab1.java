7dae9865761c25ff65c055bea19ca77e52a82509
Kserhi <movie.to.48@gmail.com>
2023-02-22 12:49:01 +0200

    Lab1

---
+++ src/Lab1.java
@@ -0,0 +1,42 @@
+
+
+public class Lab1 {
+    public static int[] insertionSort(int[] my_list) {
+        for (int j = 1; j < my_list.length; j++) {
+            int key = my_list[j];
+            int i = j-1;
+
+            while (i > -1 && my_list[i] >= key) {
+                my_list[i+1]=my_list[i];
+                i=i-1;
+            }
+            my_list[i+1]=key;
+        }
+
+        return my_list;
+    }
+
+
+    public static void main(String[] args) {
+
+        int my_list[] = new int[10];
+
+        for (int i = 0; i < my_list.length; i++) {
+            my_list[i] = (int) (Math.random()*51+50);
+        }
+        for (int i = 0; i < my_list.length; i++) {
+            System.out.println(my_list[i]);
+        }
+
+
+
+        Lab1.insertionSort(my_list);
+
+        System.out.println();
+
+        for (int i = 0; i < my_list.length; i++) {
+            System.out.println(my_list[i]);
+        }
+
+    }
+}
